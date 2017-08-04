d8ce77b039b7cee254af89d02f47fd393d96d32b
Stanislav Yusko <s.yusko@aimprosoft.com>
2017-08-04 18:55:47 +0300

    # Reading props works fine

---
+++ fill-pdf-form-plugin-platform-jar/src/main/java/com/aimprosoft/fill/pdf/form/plugin/service/pdf/form/PdfFormService.java
@@ -0,0 +1,14 @@
+package com.aimprosoft.fill.pdf.form.plugin.service.pdf.form;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+
+public interface PdfFormService {
+
+    void fillPdfFormFromMetaData(NodeRef nodeRef, Map<QName,Serializable> propertyMap) throws IOException;
+
+}
---
+++ fill-pdf-form-plugin-platform-jar/src/main/java/com/aimprosoft/fill/pdf/form/plugin/util/FillPdfModel.java
@@ -0,0 +1,9 @@
+package com.aimprosoft.fill.pdf.form.plugin.util;
+
+public final class FillPdfModel {
+
+    public static final String NAMESPACE_URI = "http://www.aimprosoft.com/model/content/1.0";
+
+    public static final String TYPE = "document";
+
+}
