be07c56055719f44a30bd0c4a93aefab868fa6b9
Stanislav Yusko <s.yusko@aimprosoft.com>
2017-08-07 14:49:47 +0300

    # Comments

--- fill-pdf-form-plugin-platform-jar/src/main/java/com/aimprosoft/fill/pdf/form/plugin/service/FillPdfModelBehaviorHandler.java
+++ fill-pdf-form-plugin-platform-jar/src/main/java/com/aimprosoft/fill/pdf/form/plugin/service/FillPdfModelBehaviorHandler.java
@@ -9,18 +9,84 @@ import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.pdfbox.exceptions.COSVisitorException;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * <p>Implementation of {@code NodeServicePolicies.OnUpdatePropertiesPolicy}</p>
+ *
+ * <p>When the properties updates the {@link FillPdfModelBehaviorHandler#onUpdateProperties(NodeRef, Map, Map)}
+ * via the {@link PdfFormService} fills values in the <span>PDF-document</span>
+ * updates values in </p>
+ *
+ * @see NodeServicePolicies.OnUpdatePropertiesPolicy
+ */
 public class FillPdfModelBehaviorHandler implements NodeServicePolicies.OnUpdatePropertiesPolicy {
 
-    //Dependencies
+    /* Dependencies */
     private PolicyComponent policyComponent;
     private PdfFormService pdfFormService;
 
+    /* Behaviours */
+    private Behaviour updateBehaviour;
+
+    /* Static fields */
+    private final static String UPDATE_PROPERTIES = "updateBehaviour";
+
+    /* Messages */
+    private final static String ERROR_MESSAGE = "Filling PDF with fields failed, more info: ";
+    private final static String LOG_MESSAGE = "Filling PDF with messages finished OK";
+
+    /* Logger */
+    private static Logger logger = Logger.getLogger(FillPdfModelBehaviorHandler.class);
+
+    /**
+     * Creates {@link Behaviour this.updateBehavviour}
+     * and binds {@link PolicyComponent this.policyComponent}
+     * to the behaviour
+     */
+    public void init() {
+        this.updateBehaviour = new JavaBehaviour(
+                this,
+                UPDATE_PROPERTIES,
+                Behaviour.NotificationFrequency.TRANSACTION_COMMIT
+        );
+
+        this.policyComponent.bindClassBehaviour(
+                QName.createQName(NamespaceService.ALFRESCO_URI, UPDATE_PROPERTIES),
+                QName.createQName(FillPdfModel.NAMESPACE_URI, FillPdfModel.TYPE),
+                this.updateBehaviour
+        );
+
+    }
+
+    /**
+     * Called after a node's properties have been changed.
+     * After the properties has been set - the actual work is being done
+     * via {@link PdfFormService#fillPdfFormFromMetaData(NodeRef, Map)}
+     *
+     * @param nodeRef reference to the updated node
+     * @param oldProp the node's properties before the change
+     * @param newProp the node's properties after the change
+     *
+     * @see PdfFormService
+     */
+    @Override
+    public void onUpdateProperties(NodeRef nodeRef, Map<QName, Serializable> oldProp, Map<QName, Serializable> newProp) {
+        try {
+            pdfFormService.fillPdfFormFromMetaData(nodeRef, newProp);
+        } catch (IOException | COSVisitorException e) {
+            logger.error(ERROR_MESSAGE + e.getMessage());
+        }
+        logger.log(Level.INFO, LOG_MESSAGE);
+    }
+
+    /* Setters */
     public void setPdfFormService(PdfFormService pdfFormService) {
         this.pdfFormService = pdfFormService;
     }
@@ -29,30 +95,7 @@ public class FillPdfModelBehaviorHandler implements NodeServicePolicies.OnUpdate
         this.policyComponent = policyComponent;
     }
 
-    // Behaviours
-    private Behaviour onUpdateProperties;
-
-    public void init(){
-        this.onUpdateProperties = new JavaBehaviour(this,"onUpdateProperties",
-                Behaviour.NotificationFrequency.TRANSACTION_COMMIT);
-
-        this.policyComponent.bindClassBehaviour(
-                QName.createQName(NamespaceService.ALFRESCO_URI, "onUpdateProperties"),
-                QName.createQName(FillPdfModel.NAMESPACE_URI, FillPdfModel.TYPE),
-                this.onUpdateProperties
-        );
-
-    }
-
-    @Override
-    public void onUpdateProperties(NodeRef nodeRef, Map<QName, Serializable> oldProp, Map<QName, Serializable> newProp) {
-        try {
-            pdfFormService.fillPdfFormFromMetaData(nodeRef, newProp);
-        } catch (IOException | COSVisitorException e) {
-            e.printStackTrace();
-        }
-        System.out.println("UPDATING PDF FINISHED");
-    }
+    /* !Setters */
 }
 
 
--- fill-pdf-form-plugin-platform-jar/src/main/java/com/aimprosoft/fill/pdf/form/plugin/service/pdf/form/PdfFormService.java
+++ fill-pdf-form-plugin-platform-jar/src/main/java/com/aimprosoft/fill/pdf/form/plugin/service/pdf/form/PdfFormService.java
@@ -8,8 +8,28 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * <h2>Service that fills <span>PDF-document</span> with values.</h2>
+ *
+ * <p>The PDF should have input fields with unique IDs,
+ * that this service uses to get values from Alfresco properties.</p>
+ *
+ * <p>The only method {@code fillPdfFormFromMetaData()} does all job for
+ * filling data.</p>
+ *
+ */
 public interface PdfFormService {
 
+    /**
+     * {@code fillPdfFormFromMetaData()} fills values to <span>PDF-document</span>'s
+     * fields from the form with {@code propertyMap}'s values.
+     *
+     * @param nodeRef nodeRef of the given <span>PDF-document</span>
+     * @param propertyMap map of the values(properties) to fill to the <span>PDF-document</span>
+     * @throws IOException thrown when problems occurred with I/O in Alfresco repository
+     * @throws COSVisitorException thrown when problems occurred with {@link org.apache.pdfbox.pdmodel.PDDocument}
+     *
+     */
     void fillPdfFormFromMetaData(NodeRef nodeRef, Map<QName,Serializable> propertyMap) throws IOException, COSVisitorException;
 
 }
