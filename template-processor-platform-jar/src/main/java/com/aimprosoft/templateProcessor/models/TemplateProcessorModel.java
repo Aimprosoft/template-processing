bdaf82c1755ff22dc08ebc545e5875a85d889294
Stanislav Yusko <s.yusko@aimprosoft.com>
2017-08-23 11:56:34 +0300

    # Code refactoring

--- template-processor-platform-jar/src/main/java/com/aimprosoft/templateProcessor/models/TemplateProcessorModel.java
+++ template-processor-platform-jar/src/main/java/com/aimprosoft/templateProcessor/models/TemplateProcessorModel.java
@@ -7,9 +7,9 @@ import org.alfresco.service.namespace.QName;
  */
 public final class TemplateProcessorModel {
     public final static String NAMESPACE_URI = "http://www.aimprosoft.com/model/content/1.0";
-    public final static String PROPERTY_LOCAL_NAME = "processed";
 
-    public final static QName ASPECT_PROPERTY = QName.createQName(NAMESPACE_URI, PROPERTY_LOCAL_NAME);
+    public final static QName ASPECT_TEMPLATE = QName.createQName(NAMESPACE_URI, "template");
+    public final static QName PROP_PROCESSED = QName.createQName(NAMESPACE_URI, "processed");
 
     private TemplateProcessorModel(){}
 }
